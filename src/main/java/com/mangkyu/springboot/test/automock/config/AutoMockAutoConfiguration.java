e0f8abb03c1610bc1fbf3444149439f845a90660
MangKyu <whalsrb1226@gmail.com>
2022-04-04 23:48:15 +0900

    Add AutoConfiguration with properties and factories

---
+++ src/main/java/com/mangkyu/springboot/test/automock/config/AutoMockAutoConfiguration.java
@@ -0,0 +1,27 @@
+package com.mangkyu.springboot.test.automock.config;
+
+import com.mangkyu.springboot.test.automock.beanfactorypostprocessor.AutoMockBeanFactoryPostProcessor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+@Configuration
+@ConditionalOnMissingBean(name = AutoMockAutoConfiguration.AUTO_MOCK_BEAN_NAME, search = SearchStrategy.CURRENT)
+@ConditionalOnProperty(name = "com.mangkyu.springboot.test.automock", havingValue = "true")
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
+public class AutoMockAutoConfiguration {
+
+    public static final String AUTO_MOCK_BEAN_NAME = "autoMockBeanFactoryPostProcessor";
+
+    @Bean
+    public AutoMockBeanFactoryPostProcessor autoMockBeanFactoryPostProcessor() {
+        return new AutoMockBeanFactoryPostProcessor();
+    }
+
+}
