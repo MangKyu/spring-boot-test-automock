0f6715ae45444089de8e98e0eaf23eed2f402bef
MangKyu <whalsrb1226@gmail.com>
2022-04-04 23:22:21 +0900

    Add AutoMockClassParser

---
+++ src/main/java/com/mangkyu/springboot/test/automock/parser/AutoMockClassParser.java
@@ -0,0 +1,50 @@
+package com.mangkyu.springboot.test.automock.parser;
+
+import com.mangkyu.springboot.test.automock.listener.AutoMockTestExecutionListener;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.mockito.internal.util.MockUtil;
+import org.springframework.test.context.TestContext;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@UtilityClass
+public final class AutoMockClassParser {
+
+    public static Constructor<?> findConstructor(final String beanClassName) {
+        try {
+            final Class<?> beanClass = AutoMockClassParser.class.getClassLoader().loadClass(beanClassName);
+            return beanClass.getDeclaredConstructors()[0];
+        } catch (final ClassNotFoundException e) {
+            log.debug("cannot find class: {}", beanClassName, e);
+        }
+        return AutoMockTestExecutionListener.class.getDeclaredConstructors()[0];
+    }
+
+    public static List<Object> findMockedFieldVariables(final TestContext testContext) {
+        return Arrays.stream(findDeclaredFields(testContext))
+                .peek(v -> v.setAccessible(true))
+                .map(v -> findFieldVariables(testContext, v))
+                .filter(MockUtil::isMock)
+                .collect(Collectors.toList());
+    }
+
+    private static Field[] findDeclaredFields(final TestContext testContext) {
+        return testContext.getTestClass().getDeclaredFields();
+    }
+
+    private static Object findFieldVariables(final TestContext testContext, final Field field) {
+        try {
+            return field.get(testContext.getTestInstance());
+        } catch (IllegalAccessException e) {
+            log.debug("cannot findFieldVariables: {}", field.getName(), e);
+        }
+        return null;
+    }
+
+}
---
+++ src/test/java/com/mangkyu/springboot/test/automock/parser/AutoMockClassParserTest.java
@@ -0,0 +1,48 @@
+package com.mangkyu.springboot.test.automock.parser;
+
+import com.mangkyu.springboot.test.automock.testcontext.AutoMockTestContext;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestContext;
+
+import java.lang.reflect.Constructor;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AutoMockClassParserTest {
+
+    private TestContext testContext;
+
+    @BeforeEach
+    void init() {
+        testContext = new AutoMockTestContext();
+    }
+
+    @Test
+    void findConstructorSuccess() {
+        final Constructor<?> constructor = AutoMockClassParser.findConstructor(TestConstructorClass.class.getName());
+        assertThat(constructor.getParameterTypes().length).isOne();
+    }
+
+    @Test
+    void findConstructorSuccess_NotExistsClass() {
+        final Constructor<?> constructor = AutoMockClassParser.findConstructor("NotExists");
+        assertThat(constructor.getParameterTypes().length).isZero();
+    }
+
+    @Test
+    void findMockedFieldVariables() {
+        final List<Object> fieldList = AutoMockClassParser.findMockedFieldVariables(testContext);
+        assertThat(fieldList.size()).isEqualTo(1);
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    private static class TestConstructorClass {
+        private final String name;
+    }
+
+}
