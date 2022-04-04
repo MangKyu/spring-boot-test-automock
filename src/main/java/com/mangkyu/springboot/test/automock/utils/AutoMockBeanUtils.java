dafb93bb48b8f9f47146a7c8bc7e4cb6689e017a
MangKyu <whalsrb1226@gmail.com>
2022-04-04 23:24:24 +0900

    Add logs on AutoMockBeanUtils

--- src/main/java/com/mangkyu/springboot/test/automock/utils/AutoMockBeanUtils.java
+++ src/main/java/com/mangkyu/springboot/test/automock/utils/AutoMockBeanUtils.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.mock;
 public final class AutoMockBeanUtils {
 
     public static void registerSingletonMock(final ConfigurableListableBeanFactory beanFactory, final Class<?> parameterType, final String beanName) {
+        log.debug("beanName: {} is registered Singleton", beanName);
         beanFactory.registerSingleton(beanName, mock(parameterType));
     }
 
