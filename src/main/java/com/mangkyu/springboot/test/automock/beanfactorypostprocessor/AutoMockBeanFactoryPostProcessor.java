3efe91a894c86d81ea73becd111137f7fb54051b
MangKyu <whalsrb1226@gmail.com>
2022-04-04 23:43:56 +0900

    Add AutoMockBeanFactoryPostProcessor

---
+++ src/main/java/com/mangkyu/springboot/test/automock/beanfactorypostprocessor/AutoMockBeanFactoryPostProcessor.java
@@ -0,0 +1,45 @@
+package com.mangkyu.springboot.test.automock.beanfactorypostprocessor;
+
+import com.mangkyu.springboot.test.automock.parser.AutoMockClassParser;
+import com.mangkyu.springboot.test.automock.utils.AutoMockBeanUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.ScannedGenericBeanDefinition;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class AutoMockBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(final ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        log.trace("postProcessBeanFactory start");
+        final List<Constructor<?>> constructorList = findConstructorsOfScannedBean(beanFactory);
+
+        for (Constructor<?> v : constructorList) {
+            for (Class<?> aClass : v.getParameterTypes()) {
+                final String beanName = AutoMockBeanUtils.generateDefaultBeanName((DefaultListableBeanFactory) beanFactory, aClass);
+                if (!beanFactory.containsBean(beanName)) {
+                    AutoMockBeanUtils.registerSingletonMock(beanFactory, aClass, beanName);
+                }
+            }
+        }
+
+        log.trace("postProcessBeanFactory complete");
+    }
+
+    public List<Constructor<?>> findConstructorsOfScannedBean(final ConfigurableListableBeanFactory beanFactory) {
+        return Arrays.stream(beanFactory.getBeanDefinitionNames())
+                .map(beanFactory::getBeanDefinition)
+                .filter(v -> v instanceof ScannedGenericBeanDefinition)
+                .map(v -> AutoMockClassParser.findConstructor(v.getBeanClassName()))
+                .collect(Collectors.toList());
+    }
+
+}
