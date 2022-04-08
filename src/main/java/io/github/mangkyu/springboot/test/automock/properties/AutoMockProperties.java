55a93aa46f9be8eb2cade78171f6c54a14a4b8b1
MangKyu <whalsrb1226@gmail.com>
2022-04-08 18:36:57 +0900

    Add AutoMockProperties

---
+++ src/main/java/io/github/mangkyu/springboot/test/automock/properties/AutoMockProperties.java
@@ -0,0 +1,12 @@
+package io.github.mangkyu.springboot.test.automock.properties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class AutoMockProperties {
+
+    private final String basePackage;
+
+}
